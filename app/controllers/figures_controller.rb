01a5000069f788b8bddbd43ca8cbe0bc83edf85b
Adam S <adam.sarli@gmail.com>
2019-05-12 20:03:21 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -36,7 +36,7 @@ class FiguresController < ApplicationController
     @figure = Figure.find(params[:id])
     @figure.update(params[:figure])
     @figure.titles << Title.find_or_create_by(name: params[:title][:name]) if !params[:title][:name].empty?
-    @figure.landmarks << Landmark.find_or_create_by(name: params[:landmark][:name]) if !params[:landmark][:name].empty?
+    @figure.landmarks << Landmark.find_or_create_by(name: params[:landmark][:name], year_completed: params[:landmark][:year]) if !params[:landmark][:name].empty?
     @figure.save
     redirect "/figures/#{@figure.id}"
   end
